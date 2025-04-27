8953cd85bafd01dd9cde0e87a4ad495d78bee836
Michael Dold <micha.dold@gmail.com>
2025-04-27 20:17:52 +0000

    fix(docs): detect GitHub Actions environment to handle mermaid.js loading correctly

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,6 @@
-# Configuration file for the Sphinx documentation builder.
+import os
+
+# -- Project information -----------------------------------------------------
 
 project = 'Python Starter Template'
 copyright = '2025, Michael Dold'
@@ -6,18 +8,31 @@ author = 'Michael Dold'
 
 release = '0.1.0'
 
+# -- General configuration ---------------------------------------------------
+
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
-    'sphinxcontrib.mermaid'
+    'sphinxcontrib.mermaid',
 ]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
+# -- HTML output --------------------------------------------------------------
+
 html_theme = 'alabaster'
 html_static_path = ['_static']
 
-# Mermaid configuration (optional tweak)
-mermaid_version = "9.1.3"
+# -- Mermaid configuration ---------------------------------------------------
+
+if os.getenv("GITHUB_ACTIONS") == "true":
+    # Running in GitHub Actions — prevent sphinxcontrib-mermaid from installing
+    mermaid_output_format = "raw"
+    html_js_files = [
+        'js/mermaid.min.js',
+    ]
+else:
+    # Running locally — allow automatic management
+    mermaid_output_format = "png"
